862ec9ce7def128c5c5ed4701480cc83b29e17d8
Nebenmor <nebenmor.anthony@gmail.com>
2024-12-27 18:54:43 +0100

    implement settings menu behaviour

---
+++ script.js
@@ -0,0 +1,6 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
+
+function settingsMenuToggle() {
+    settingsmenu.classList.toggle("settings-menu-height");
+}
